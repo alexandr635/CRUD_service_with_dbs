70061486013aca2844ef6395b7e05cce83e9c6fb
Aleksandr <aleksandr@localhost.localdomain>
2021-10-31 15:54:18 +0300

    init postgre branch

--- src/main/java/com/example/crud/Model/Data.java
+++ src/main/java/com/example/crud/Model/Data.java
@@ -2,8 +2,7 @@ package com.example.crud.Model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.sql.Timestamp;
+import java.util.Date;
 import java.util.UUID;
 
 @Document
@@ -11,5 +10,5 @@ public class Data {
     @Id
     public UUID id;
     public String data;
-    public Timestamp time;
+    public Date time;
 }
--- src/main/java/com/example/crud/Repository/IMongoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.crud.Repository;
-
-import com.example.crud.Model.Data;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface IMongoRepository extends MongoRepository<Data, UUID> {
-}
